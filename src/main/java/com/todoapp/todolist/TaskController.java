731870c6a84a0131060e95029d03649f439938dc
Hylke <hylkest2002@gmail.com>
2025-09-29 23:42:06 +0200

    Add delete method

--- src/main/java/com/todoapp/todolist/TaskController.java
+++ src/main/java/com/todoapp/todolist/TaskController.java
@@ -2,10 +2,9 @@
 package com.todoapp.todolist;
 
 // Springboot packages importeren
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,14 +14,31 @@ import java.util.List;
 // Dus als iemand het adres bezoekt met /api/tasks komt hij hierin
 @RequestMapping("/api/tasks")
 public class TaskController {
-//    Get mapping zorgt ervoor dat deze methode wordt aangeroepen bij een GET verzoek
-    @GetMapping
-//    Hier een functie die een lijst met strings teruggeeft
-    public List<Task> getTasks() {
-        return List.of(
+    private List<Task> tasks = new ArrayList<>(List.of(
             new Task(1L, "Takenlijst maken", true),
             new Task(2L, "Spring boot leren", false),
             new Task(3L, "REST API bouwen", false)
-        );
+    ));
+
+//        Get mapping zorgt ervoor dat deze methode wordt aangeroepen bij een GET verzoek
+    @GetMapping
+//    Hier een functie die een lijst met strings teruggeeft
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+//    Bij een POST verzoek naar /api/tasks wordt dit uitgevoerd
+    @PostMapping
+//    @RequestBody: vertelt Spring Boot dat de JSON-inhoud van het verzoek moet
+//    worden omgezet naar een Task object
+    public void addTask(@RequestBody Task newTask) {
+        tasks.add(newTask);
+    }
+//  @DeleteMapping("/{id}"): deze methode wordt aangeroepen als iemand
+//  een DELETE-request doet naar /api/tasks/1
+    @DeleteMapping("/{id}")
+//    @PathVariable Long id: het stukje {id} uit de URL wordt gekoppeld aan de parameter id
+    public void deleteTask(@PathVariable Long id) {
+        tasks.removeIf(task -> task.getId().equals(id));
     }
 }
